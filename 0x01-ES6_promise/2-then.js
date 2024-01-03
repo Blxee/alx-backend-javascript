0837b56698326283323f7204a8324a643b09b58e
Blxee <aminedev57@gmail.com>
2024-01-03 01:44:48 +0100

    Fixed: task **2. Catch me if you can** of **0x01-ES6_promise**

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .then(() => console.log('Got a response from the API'))
-    .catch(() => new Error());
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
