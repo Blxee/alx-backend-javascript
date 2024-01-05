77ab9a67f1388149dfef7ae4b75b9597c951285f
Blxee <aminedev57@gmail.com>
2024-01-05 02:03:16 +0100

    Fixed: task '5. A Building' of '0x02-ES6_classes'

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (!('evacuationWarningMessage' in this)) {
+    if (!('evacuationWarningMessage' in this && typeof this.evacuationWarningMessage === 'function')) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
