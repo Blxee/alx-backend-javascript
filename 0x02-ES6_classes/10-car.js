c20211cc6f377efc37adb7fc3a9f810546e539c4
Blxee <aminedev57@gmail.com>
2024-01-05 01:44:17 +0100

    Fixed: task '10. Vroom' of '0x02-ES6_classes'

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,7 +6,7 @@ export default class Car {
   }
 
   cloneCar() {
-    const newCar = new this.constructor[Symbol.species]();
-    return newCar;
+    const constructor = this.constructor[Symbol.species] || this.constructor;
+    return new constructor();
   }
 }
