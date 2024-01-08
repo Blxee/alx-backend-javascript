b9f18d589246a6bdf2efef07d685387083f16ed1
Blxee <aminedev57@gmail.com>
2024-01-08 14:06:21 +0100

    Added: task (3. Reduce) of 0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,4 @@
 export default function getStudentIdsSum(array) {
   return array
-    .map((student) => student.id)
-    .reduce((acc, id) => acc + id);
+    .reduce((acc, student) => acc + student.id, 0);
 }
