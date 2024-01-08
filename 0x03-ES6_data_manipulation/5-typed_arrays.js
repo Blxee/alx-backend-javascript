4a7c642412714755d3153f01a2c190980e373e58
Blxee <aminedev57@gmail.com>
2024-01-08 17:19:37 +0100

    Fixed: task (5. Typed Arrays) of 0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const arrayBuffer = new ArrayBuffer(length);
   const dataView = new DataView(arrayBuffer);
-  dataView.setInt8(position, value);
-  return arrayBuffer;
+  try {
+    dataView.setInt8(position, value);
+  } catch (e) {
+    throw new Error('Position outside range');
+  }
+  return dataView;
 }
