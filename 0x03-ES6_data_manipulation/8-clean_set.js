73ab3e3fa60452884a90c3dd63f2d74d7bc8d850
Blxee <aminedev57@gmail.com>
2024-01-09 21:34:37 +0100

    Changed: task (8. Clean set) of 0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(inputSet, startString) {
-  if (!startString) return '';
+  if (!startString || !inputSet || typeof startString !== 'string') {
+    return '';
+  }
   const arr = new Array(...inputSet)
     .filter((str) => str.startsWith(startString))
     .map((str) => str.substring(startString.length));
