fb16dacbc144a1822129dc5bb189a3728ff773c2
Blxee <aminedev57@gmail.com>
2024-01-09 21:24:59 +0100

    Revert "Changed: task (8. Clean set) of 0x03-ES6_data_manipulation"
    This reverts commit 2bb88d4a2c9350c25e7793e42359d8546e564708.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, prefix) {
+  if (!prefix) return '';
   const arr = new Array(...set)
     .filter((str) => str.startsWith(prefix))
     .map((str) => str.replace(prefix, ''));
