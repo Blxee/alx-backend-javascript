9dd2b1b6f6579b699623900ec2633d6c3e480ab6
Blxee <aminedev57@gmail.com>
2024-01-09 19:22:25 +0100

    Fixed: task (11. Weak link data structure) of 0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,5 +4,5 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
   const queries = weakMap.get(endpoint) + 1;
   weakMap.set(endpoint, queries);
-  if (queries > 5) throw new Error('Endpoint load is high');
+  if (queries >= 5) throw new Error('Endpoint load is high');
 }
