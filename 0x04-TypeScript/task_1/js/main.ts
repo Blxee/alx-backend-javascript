a55fa822869772fccf0f042a484678add6e81d72
Blxee <aminedev57@gmail.com>
2024-01-10 18:30:08 +0100

    Added: task (4. Writing a class) of 0x04-TypeScript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,34 @@ interface PrintTeacherFunction {
 const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`;
 }
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const studentClass: StudentConstructor = StudentClass;
