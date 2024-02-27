7149bf1b280d0bc562ce8cdcce1b820c76538037
Blxee <aminedev57@gmail.com>
2024-02-27 02:54:02 +0100

    Added: task (2. Reading a file synchronously with Node JS) for 0x05. Node JS basic

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+
+  let data = fs.readFileSync(path).toString();
+  data = data.replace(/.+\n/, '');
+
+  const studentsLen = data.match(/\n/g).length;
+  console.log(`Number of students: ${studentsLen}`);
+
+  const fields = new Set(data.match(/[^,]+(?=\n)/g));
+
+  for (const field of fields) {
+    process.stdout.write(`Number of students in ${field}:`);
+    let lines = data.split('\n').slice(0, -1);
+
+    lines = lines
+      .filter((line) => line.match(field))
+      .map((line) => line.match(/[^,]+/).toString());
+
+    process.stdout.write(` ${lines.length}.`);
+    console.log(` List: ${lines.join(', ')}`);
+  }
+};
