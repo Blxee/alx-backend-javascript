c76ecf7292bb5fad7d7f21d5ce95966963afbd61
Blxee <aminedev57@gmail.com>
2024-02-28 02:04:52 +0100

    Added: task (7. Create a more complex HTTP server using Express) for 0x05. Node JS basic

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs = require('fs');
+const countStudents = require('./2-read_file');
 
 const app = express();
 
@@ -9,29 +9,8 @@ app.get('/', (_req, res) => {
 
 app.get('/students', (_req, res) => {
   const path = process.argv[2];
-  if (!fs.existsSync(path)) {
-    throw new Error('Cannot load the database');
-  }
-
-  let data = fs.readFileSync(path).toString();
-  data = data.replace(/.+\n/, '').trim().concat('\n');
-
-  const studentsLen = data.match(/\n/g).length;
-  res.write(`Number of students: ${studentsLen}\n`);
-
-  const fields = new Set(data.match(/[^,]+(?=\n)/g));
-
-  for (const field of fields) {
-    res.write(`Number of students in ${field}:`);
-    let lines = data.split('\n').slice(0, -1);
-
-    lines = lines
-      .filter((line) => line.match(field))
-      .map((line) => line.match(/[^,]+/).toString());
-
-    res.write(` ${lines.length}.`);
-    res.write(` List: ${lines.join(', ')}\n`);
-  }
+  countStudents(path, res);
+  res.end();
 });
 
 app.listen(1245);
