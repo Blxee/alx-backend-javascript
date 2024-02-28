a37d53e320f7a90af37d6417c80f7c63d56decd2
Blxee <aminedev57@gmail.com>
2024-02-28 02:13:01 +0100

    Fixing: task (1. Using Process stdin) for 0x05. Node JS basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,6 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-  process.stdin.pause();
   console.log('This important software is now closing');
+  process.stdin.pause();
 });
