727709ee225f101dd4fff699cdc27efb8879cb8e
Blxee <aminedev57@gmail.com>
2024-02-28 02:00:14 +0100

    Fixing: task (5. Create a more complex HTTP server using Node's HTTP module) for 0x05. Node JS basic

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,6 +4,7 @@ const countStudents = require('./2-read_file');
 module.exports = http.createServer((req, res) => {
   if (req.url === '/students') {
     const path = process.argv[2];
+    res.write('This is the list of our students\n');
     countStudents(path, res);
   } else {
     res.write('Hello Holberton School!');
