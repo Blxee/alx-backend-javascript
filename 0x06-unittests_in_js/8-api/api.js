bda049bdb88a6b34c05d01272c1beec2c8720e29
Blxee <aminedev57@gmail.com>
2024-02-29 23:51:39 +0100

    Added: task (9. Regex integration testing) of 0x06. Unittests in JS

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.status(200).send('Welcome to the payment system');
 });
 
+app.get('/cart/:id(\\d+)', (req, res) => {
+  res.status(200).send(`Payment methods for cart ${req.params.id}`);
+});
+
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,35 @@
 const request = require('request');
 const assert = require('assert');
 
-describe('Test index /', function() {
-  it('Test', function(done) {
+describe('Test api', function() {
+  it('Test / route', function(done) {
     request('http://localhost:7865/', (error, response, body) => {
       if (error || !response)
         done();
 
       assert(response.statusCode === 200);
       assert(body === 'Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Test /cart/<id> route invalid', function(done) {
+    request('http://localhost:7865/cart/not', (error, response, body) => {
+      if (error || !response)
+        done();
+
+      assert(response.statusCode === 404);
+      done();
+    });
+  });
+
+  it('Test /cart/<id> route valid', function(done) {
+    request('http://localhost:7865/cart/89', (error, response, body) => {
+      if (error || !response)
+        done();
+
+      assert(response.statusCode === 200);
+      assert(body === 'Payment methods for cart 89');
       done();
     });
   });
