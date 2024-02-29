98994f1a5077a986b1d7dff1212885103eb237c3
Blxee <aminedev57@gmail.com>
2024-03-01 00:42:39 +0100

    Fixing: task (6. Async tests with done) of 0x06. Unittests in JS

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ describe('getPaymentTokenFromAPI', function() {
   it('Test result of getPaymentTokenFromAPI', (done) => {
     getPaymentTokenFromAPI(true)
       .then((res) => {
-        // assert.equal(res, { data: 'Successful response from the API' });
+        assert.deepEqual(res, { data: 'Successful response from the API' });
         done();
       });
   });
