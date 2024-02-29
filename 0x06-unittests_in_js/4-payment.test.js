34ce08c9d5faee6e79f5952b79f006f086c69201
Blxee <aminedev57@gmail.com>
2024-02-29 22:43:04 +0100

    Added: task (4. Stubs) of 0x06. Unittests in JS

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment')
+const sendPaymentRequestToApi = require('./4-payment')
 
 describe('Test suit for sendPaymentRequestToApi function', function() {
   it('Test that calculateNumber is called', function() {
