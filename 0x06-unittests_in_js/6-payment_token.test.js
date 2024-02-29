0c55f99499eb038954d4f69ec1ff459ddce33d75
Blxee <aminedev57@gmail.com>
2024-02-29 23:13:39 +0100

    Added: task (6. Async tests with done) of 0x06. Unittests in JS

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const assert = require('assert');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('Test result of getPaymentTokenFromAPI', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        assert.equal(res, { data: 'Successful response from the API' });
+        done();
+      });
+  });
+});
