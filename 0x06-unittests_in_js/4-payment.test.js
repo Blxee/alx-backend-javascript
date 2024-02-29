bf4510ac64196c96b6eaab9f594a4a283ef89886
Blxee <aminedev57@gmail.com>
2024-02-29 22:40:58 +0100

    Added: task (4. Stubs) of 0x06. Unittests in JS

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment')
+
+describe('Test suit for sendPaymentRequestToApi function', function() {
+  it('Test that calculateNumber is called', function() {
+    const calculateNumber = sinon.stub(Utils, 'calculateNumber');
+    calculateNumber.returns(10);
+    const log = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(calculateNumber, 'SUM', 100, 20);
+    sinon.assert.calledWith(log, 'The total is: 10');
+
+    calculateNumber.restore();
+    log.restore();
+  });
+});
