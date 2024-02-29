842788b6dd4bdfc3c44fda15ddfb91bc80c2119e
Blxee <aminedev57@gmail.com>
2024-02-29 15:01:26 +0100

    Added: task (0. Basic test with Mocha and Node assertion library) of 0x06. Unittests in JS

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,9 @@
+import calculateNumber from './0-calcul.js';
+import { assert } from 'chai';
+
+describe('Test suit for calculateNumber', function() {
+  it('Test round sum for calculateNumber', function() {
+    assert(calculateNumber(5.1, 4.8) === 10, '5.1 + 4.8 is not 10');
+    assert(calculateNumber(5.8, 4.1) === 10, '5.8 + 4.1 is not 10');
+  });
+});
