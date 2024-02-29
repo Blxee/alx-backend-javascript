d3ac6c7681f9ed58df0eaa330448b455e9a030b7
Blxee <aminedev57@gmail.com>
2024-02-29 21:05:45 +0100

    Added: task (2. Basic test using Chai assertion library) of 0x06. Unittests in JS

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,8 @@
 const calculateNumber = require('./2-calcul_chai.js');
-const { expect } = require('chai');
+let expect;
+import('chai').then((res) => {
+  expect = res.expect;
+});
 
 describe('Test suit for calculateNumber', function() {
   it('Test round sum for calculateNumber', function() {
@@ -12,7 +15,7 @@ describe('Test suit for calculateNumber', function() {
   });
   it('Test round divide for calculateNumber', function() {
     expect(calculateNumber('DIVIDE', 5.1, 4.8)).to.equal(1);
-    expect(calculateNumber('DIVIDE', 5, 2)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 5, 2)).to.equal(2.5);
     expect(calculateNumber('DIVIDE', 3, 0)).to.equal('Error');
   });
 });
