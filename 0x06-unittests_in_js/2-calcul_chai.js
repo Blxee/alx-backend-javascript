dc4833ad3a7a40dba0fbf7962cad1559d4b6a533
Blxee <aminedev57@gmail.com>
2024-02-29 20:37:21 +0100

    Added: task (2. Basic test using Chai assertion library) of 0x06. Unittests in JS

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,16 @@
+module.exports = function calculateNumber(type, a, b) {
+  a = Math.round(a);
+  b = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return a + b;
+    case 'SUBTRACT':
+      return a - b;
+    case 'DIVIDE':
+      if (b === 0)
+        return 'Error';
+      else
+        return a / b;
+  }
+}
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,18 @@
+const calculateNumber = require('./2-calcul_chai.js');
+const { expect } = require('chai');
+
+describe('Test suit for calculateNumber', function() {
+  it('Test round sum for calculateNumber', function() {
+    expect(calculateNumber('SUM', 5.1, 4.8)).to.equal(10);
+    expect(calculateNumber('SUM', 5.8, 4.1)).to.equal(10);
+  });
+  it('Test round subtract for calculateNumber', function() {
+    expect(calculateNumber('SUBTRACT', 5.1, 4.8)).to.equal(0);
+    expect(calculateNumber('SUBTRACT', 5.8, 4.1)).to.equal(2);
+  });
+  it('Test round divide for calculateNumber', function() {
+    expect(calculateNumber('DIVIDE', 5.1, 4.8)).to.equal(1);
+    expect(calculateNumber('DIVIDE', 5, 2)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 3, 0)).to.equal('Error');
+  });
+});
