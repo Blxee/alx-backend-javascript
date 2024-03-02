e61fa286b291b79d8650f23db031c2fab27904aa
Blxee <aminedev57@gmail.com>
2024-03-02 02:14:38 +0100

    Revert "Fixing: task (3. Reading a file asynchronously with Node JS) for 0x05. Node JS basic"
    This reverts commit ac09a6817bca473ca224808c6f99504ab01af336.

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,30 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, abort) => {
     if (!fs.existsSync(path)) {
-      reject(new Error('Cannot load the database'));
+      abort(new Error('Cannot load the database'));
     }
 
-    fs.readFile(path, (err, res) => {
-      const data = res.toString().replace(/.+\n/, '').trim().concat('\n');
+    let data = fs.readFileSync(path).toString();
+    data = data.replace(/.+\n/, '').trim().concat('\n');
 
-      const studentsLen = data.match(/\n/g).length;
-      console.log(`Number of students: ${studentsLen}`);
+    const studentsLen = data.match(/\n/g).length;
+    console.log(`Number of students: ${studentsLen}`);
 
-      const fields = new Set(data.match(/[^,]+(?=\n)/g));
+    const fields = new Set(data.match(/[^,]+(?=\n)/g));
 
-      for (const field of fields) {
-        process.stdout.write(`Number of students in ${field}:`);
-        let lines = data.split('\n');
+    for (const field of fields) {
+      process.stdout.write(`Number of students in ${field}:`);
+      let lines = data.split('\n');
 
-        lines = lines
-          .filter((line) => line.match(field))
-          .map((line) => line.match(/[^,]+/).toString());
+      lines = lines
+        .filter((line) => line.match(field))
+        .map((line) => line.match(/[^,]+/).toString());
 
-        process.stdout.write(` ${lines.length}.`);
-        console.log(` List: ${lines.join(', ')}`);
-      }
-      resolve();
-    });
+      process.stdout.write(` ${lines.length}.`);
+      console.log(` List: ${lines.join(', ')}`);
+    }
+    resolve();
   });
 };
